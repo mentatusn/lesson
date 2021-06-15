b6dc8289cb1692a0fed26fcbaca827690962deda
Andej Nesterenko <mentatus.n@gmail.com>
2021-06-15 20:32:52 +0300

    Добавили задание

--- app/src/main/java/keyone/keytwo/lesson2/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson2/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.CalendarView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CustomCalendarView calendarView = findViewById(R.id.calendar);
-        Log.d("mylogs", "onResume" +calendarView.getDate());
-        Log.d("mylogs", "onResume" +calendarView.getMyDate());
+
+       /* EditText youredittext = findViewById(R.id.edit);
+        youredittext.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                if (youredittext.hasFocus()) {
+                    v.getParent().requestDisallowInterceptTouchEvent(true);
+                    switch (event.getAction() & MotionEvent.ACTION_MASK){
+                        case MotionEvent.ACTION_SCROLL:
+                            v.getParent().requestDisallowInterceptTouchEvent(false);
+                            return true;
+                    }
+                }
+                return false;
+            }
+        });*/
     }
 
 }
