f0c484a129e1e37fd49720d0c3f32472da4a0f0c
Andej Nesterenko <mentatus.n@gmail.com>
2021-06-15 19:45:55 +0300

    Добавили кастомный календарь
    Нужно было перевести время из time_stump в человекочитаемый вид

--- app/src/main/java/keyone/keytwo/lesson2/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson2/MainActivity.java
@@ -3,6 +3,8 @@ package keyone.keytwo.lesson2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CustomCalendarView calendarView = findViewById(R.id.calendar);
+        Log.d("mylogs", "onResume" +calendarView.getDate());
+        Log.d("mylogs", "onResume" +calendarView.getMyDate());
     }
+
 }
